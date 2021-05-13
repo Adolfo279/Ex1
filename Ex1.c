00aa4cc46b01a4a8487b964effb3556977b5f6e9
Adolfo279 <82550177+Adolfo279@users.noreply.github.com>
2021-05-12 21:57:48 -0500

    Add files via upload

---
+++ Ex1.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+
+int main ()
+{
+
+    float primer, segundo, multiplica, division;
+    printf("\n\n Calderon Vazquez Adolfo Ángel");
+    printf("\n\n Programa 3");
+    printf("\n\n Saca el promedio de valores reales");
+    printf("\n\n Dame el primer valor real");
+    scanf("%f", &primer);
+    printf("\n\n Dame el segundo valor real");
+    scanf("%f", &segundo);
+    multiplica = primer * segundo;
+    division = primer / segundo;
+    printf("\n El producto es %f", multiplica);
+    printf("\n El cociente es %f", division);
+    return 0;
+}
